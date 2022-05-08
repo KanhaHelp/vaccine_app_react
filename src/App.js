f0b217fcfc49662ae403d8a0798cf4479032f5d4
KanhaHelp <djkanha044@gmail.com>
2022-05-08 12:57:09 +0530

    login form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Loginform from './components/form';
+
 
 function App() {
   return (
     <>
-      gggs
+      <Loginform />
     </>
   );
 }
---
+++ src/components/form.js
@@ -0,0 +1,101 @@
+import { Component } from 'react';
+import React, { useState } from 'react';
+import {
+  Form,
+  FormFeedback,
+  FormGroup,
+  FormText,
+  Label,
+  Input,
+  Button,
+} from 'reactstrap';
+
+function Loginform() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    
+    const handleChange = (event) => {
+     
+        if(event.target.name === 'email'){
+          setEmail(event.target.value); 
+        } else {
+          setPassword(event.target.value);
+        }
+        //console.log(event.target.name + ' -----'+ event.target.value);
+      };
+     
+    const validateEmail =  (e) => {
+        const emailRex =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     
+        const { validate } = this.state;
+     
+        if (emailRex.test(e.target.value)) {
+          validate.emailState = 'has-success';
+        } else {
+          validate.emailState = 'has-danger';
+        }
+     
+        this.setState({ validate });
+      }
+     
+    const submitForm = (e) => {
+        e.preventDefault();
+        // console.log('submit form');
+        console.log(`Email: ${email}`);
+        console.log(`password: ${password}`);
+
+      }
+
+    return (
+        <>
+    
+        <div className="login-form">
+      
+            <h2 className='text-danger text-center'>Sign In</h2>
+            <Form className="form" onSubmit={(e) => submitForm(e)}>
+            <FormGroup>
+                <Label>Username</Label>
+                <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="example@example.com"
+                // valid={this.state.validate.emailState === "has-success"}
+                // invalid={this.state.validate.emailState === "has-danger"}
+                value={email}
+                onChange={(e) => {
+                    // validateEmail(e);
+                    handleChange(e);
+                }}
+                />
+                <FormFeedback>
+                Uh oh! Looks like there is an issue with your email. Please input
+                a correct email.
+                </FormFeedback>
+                <FormFeedback valid>
+                That's a tasty looking email you've got there.
+                </FormFeedback>
+                <FormText>Your username is most likely your email.</FormText>
+            </FormGroup>
+            <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+                value={password}
+                onChange={(e) => handleChange(e)}
+                />
+            </FormGroup> <br/>
+            <Button className='btn btn-success'>Login</Button>
+            </Form>
+        </div>
+        </>
+    )
+}
+
+export default Loginform;
