8b6381e04258e76aac7a0ac6189e258685e985c3
KanhaHelp <djkanha044@gmail.com>
2022-05-08 14:06:05 +0530

    routes file added for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Loginform from './components/form';
-
+import React from 'react';
+import AllRoutes from './routes';
 
 function App() {
   return (
     <>
+      <AllRoutes />
       <Loginform />
     </>
   );
